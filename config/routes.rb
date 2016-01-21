ca1d13f28fb3d80bb69df08fea3ca0151b1ec3bf
Kyle Buttner <kylebuttner@gmail.com>
2016-01-21 21:40:47 +0000

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   get "features" => 'static_pages#features'
 
-  get "pricing" => 'static_pages#pricing'
+  get "signup" => 'static_pages#signup'
 
-  get "contact" => 'static_pages#contact'
+  get "login" => 'static_pages#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
